095b21a6096254056987b319c218dc1e429328ee
Eder Ignatowicz <ignatowicz@gmail.com>
2012-05-07 14:14:58 -0300

    cleanup

--- create_rule2.sql
+++
@@ -1,2 +0,0 @@
-CREATE RULE insert_holidays AS ON INSERT TO holidays DO INSTEAD
-	INSERT INTO EVENTS(title, starts) values (NEW.name, NEW.date);
--- cross_tab.sql
+++
@@ -1,1 +0,0 @@
-SELECT * FROM crosstab('SELECT extract(year from starts) as year, extract(month from starts) as month, count(*) FROM events GROUP BY year, month', 'SELECT * FROM month_count') AS (year int, jan int, feb int, mar int, apr int, may int, jun int, jul int, aug int, sep int, oct int, nov int, dec int) ORDER BY YEAR;
